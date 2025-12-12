1f43f4ab121ba560e0eef44144c36fce1f3b20d5
Marcus Sisouphanh <107326899+msisouphanh@users.noreply.github.com>
2025-12-12 11:56:46 -0500

    Added a simple navigation bar

---
+++ src/components/NavigationBar.tsx
@@ -0,0 +1,32 @@
+function NavigationBar() {
+  return (
+    <>
+      <nav className="w-full">
+        <ul className="flex space-x-6 mx-auto px-4 max-w-3xl">
+          <li>
+            <a className="text-blue-500 hover:text-blue-800" href="#">
+              Home
+            </a>
+          </li>
+          <li>
+            <a className="text-blue-500 hover:text-blue-800" href="#">
+              Projects
+            </a>
+          </li>
+          <li>
+            <a className="text-blue-500 hover:text-blue-800" href="#">
+              Link
+            </a>
+          </li>
+          <li>
+            <a className="text-gray-400 cursor-not-allowed" href="#">
+              Disabled
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default NavigationBar;

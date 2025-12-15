0ae4a96d4436b101741ca439dc76fdb5ac6f2f80
Marcus Sisouphanh <107326899+msisouphanh@users.noreply.github.com>
2025-12-15 15:18:49 -0500

    Added project cards template

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,9 +1,59 @@
+import rawProjectsData from "../projects.json";
+
+interface Project {
+  name: string;
+  description: string;
+  technologies: string[];
+  live: string | null;
+  repo: string | null;
+  image: string;
+}
+
+const projectsData = rawProjectsData.slice(0, 2) as Project[];
+
 function Projects() {
   return (
     <>
       <div className="w-full">
-        <div className="flex max-w-2xl mx-auto">
-          <h1 className="mt-8">Projects</h1>
+        <div className="max-w-2xl mx-auto px-4">
+          <h1 className="mt-8 mb-4 text-xl font-semibold">Projects</h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 max-w-2xl mx-auto">
+            {projectsData.map((item) => (
+              <div className="flex flex-col rounded-xl shadow-md border p-4">
+                <div className="h-40 bg-gray-200 rounded-lg mb-4">image</div>
+                <h1 key={item.name} className="text-lg font-semibold mb-2">
+                  {item.name}
+                </h1>
+                <p className="text-gray-600 mb-4">{item.description}</p>
+                <div className="flex flex-wrap gap-2 mb-4">
+                  {item.technologies.map((tech) => (
+                    <span className="bg-gray-100 text-gray-800 px-2 py-1 rounded">
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+                <div className="flex gap-2 mt-auto">
+                  {item.live && (
+                    <button className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600">
+                      Website
+                    </button>
+                  )}
+                  {item.repo ? (
+                    <button className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600">
+                      Source
+                    </button>
+                  ) : (
+                    <button
+                      className="bg-gray-500 text-white px-4 py-2 rounded disabled:opacity-50 disabled:cursor-not-allowed"
+                      disabled
+                    >
+                      Source
+                    </button>
+                  )}
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </>
--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -21,11 +21,11 @@ export const techStack = [
 function TechStack() {
   return (
     <div className="w-full mt-8">
-      <div className="grid grid-cols-7 gap-2 max-w-2xl mx-auto px-4">
+      <div className="grid grid-cols-4 md:grid-cols-7 gap-2 max-w-2xl mx-auto px-4">
         {techStack.map(({ name, Icon }) => (
           <figure
             key={name}
-            className="border-2 rounded-lg shadow-md w-21 h-21 flex flex-col items-center justify-center"
+            className="border-2 rounded-lg shadow-md flex flex-col items-center justify-center w-full aspect-square"
           >
             <Icon size={32} className="mb-1" />
             <figcaption className="mt-0.5 text-xs">{name}</figcaption>
